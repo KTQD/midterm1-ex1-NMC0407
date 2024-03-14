ce9244bcadf3ebbfe0c2723879635bd1bc181b3d
NMC0407 <157662004+NMC0407@users.noreply.github.com>
2024-03-14 08:13:56 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        MP3 mp3;
+        MP3 mp3 = new MP3("Best Hits","Top songs of year","MP3-001",49.99,180);
         mp3.showInfo();
 
-        Book book;
+        Book book = new Book("Java programming","Complete guide to Java programming language","Book-001",29.99,"John Doe",400,"Programming");
         book.showInfo();
 
     }
